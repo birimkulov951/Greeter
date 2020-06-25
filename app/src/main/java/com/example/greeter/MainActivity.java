abc1f3b8b4e3363946c6904c5eae30c429dee51a
Akyl <birimkulov951@gmail.com>
2020-06-25 18:39:25 +0600

    Debugged

--- app/src/main/java/com/example/greeter/MainActivity.java
+++ app/src/main/java/com/example/greeter/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
@@ -12,36 +14,53 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
+    String message;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                message = null;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                message = editText.getText().toString();
+
+            }
+        });
     }
 
     @SuppressLint("SetTextI18n")
     public void greet(View view) {
-
-        TextView editText = (TextView) findViewById(R.id.editText);
-        TextView textView = (TextView) findViewById(R.id.textView);
-
-        if (editText.getText().equals("") || editText.getText() == null){
-
-            toastShow("Please input your name and surname...");
-
+        if(message == null || message.equals("")){
+            toastShow("Please, enter your name...");
+            textView.setText("0");
         } else {
-
-            textView.setText("Hello, " + editText.getText().toString() + "!");
-
+            textView.setText("Hello, " + message + "!!!");
         }
     }
 
     private void toastShow (String message) {
 
-        Context context = getApplicationContext();
+        context = getApplicationContext();
 
         Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 0);
+        //toast.setGravity(Gravity.BOTTOM|Gravity.CENTER, 0, 0);
         toast.show();
 
     }
